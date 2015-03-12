9281b4c97a97fffbe782335e95f70c56e2c5a5eb
TejinderGill <laddi_122005@hotmail.com>
2015-03-12 18:21:03 -0400

    Book Appointment- TejinderGill
    Insert complete

---
+++ BlindRiver/Controllers/BookAppController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BlindRiver.Models;
+
+namespace BlindRiver.Controllers
+{
+    public class BookAppController : Controller
+    {
+        //
+        // GET: /BookApp/
+
+        bookappLinq bookObj = new bookappLinq();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(bookApp book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    bookObj.commitInsert(book);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ BlindRiver/Models/bookValidation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace BlindRiver.Models
+{
+    [MetadataType(typeof(bookAppValidation))]
+    public partial class book_app
+    { }
+
+    [Bind(Exclude = "id")]
+    public class bookAppValidation
+    {
+        [DisplayName("Name: ")]
+        [Required]
+        public string name { get; set; }
+
+        [DisplayName("Phone: ")]
+        [Required]
+        [RegularExpression(@"^\(\d{3}\) ?\d{3}( |-)?\d{4}|^\d{3}( |-)?\d{3}( |-)?\d{4}$")]
+        public int phone { get; set; }
+
+        [DisplayName("Email: ")]
+        [Required]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")]
+        public string email { get; set; }
+
+        [DisplayName("Date Of Appointment: ")]
+        [Required]
+        public DateTime doa { get; set; }
+
+        [DisplayName("Purpose: ")]
+        [Required]
+        public string purpose { get; set; }
+    }
+
+}
