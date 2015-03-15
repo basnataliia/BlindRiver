ff71788c5bf07ef87a9d646a45b0c7df7372ef60
jaehalee91 <jaehalee91@gmail.com>
2015-03-15 17:38:11 -0400

    FAQ - Insert

--- BlindRiver/Controllers/FAQController.cs
+++ BlindRiver/Controllers/FAQController.cs
@@ -4,17 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using BlindRiver.Models;
+
 namespace BlindRiver.Controllers
 {
+    
     public class FAQController : Controller
     {
-        //
-        // GET: /FAQ/
+        FAQLinq objFAQ = new FAQLinq();
 
         public ActionResult Index()
         {
+            var faq = objFAQ.getFAQs();
+            return View(faq);
+        }
+
+     public ActionResult Insert()
+        {
             return View();
         }
+
+     [HttpPost]
+     public ActionResult Insert (FAQ faq)
+     {
+         if (ModelState.IsValid)
+         {
+             try
+             {
+                 objFAQ.commitInsert(faq);
+                 return RedirectToAction("Index");
+             }
+             catch
+             {
+                 return View();
+             }
+         }
+         return View();
+     }
 
     }
 }
---
+++ BlindRiver/Models/FAQvalidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace BlindRiver.Models
+{
+
+    [MetadataType(typeof(FAQvalidation))]
+    public partial class FAQ
+    { 
+    }
+
+    [Bind(Exclude="id")]
+    public class FAQvalidation
+    {
+        [DisplayName ("Frequently Asked Question: ")]
+        [Required]
+        public string questions {get; set;}
+
+        [DisplayName ("Answer: ")]
+        [Required]
+        public string answers {get; set;}
+    }
+}
