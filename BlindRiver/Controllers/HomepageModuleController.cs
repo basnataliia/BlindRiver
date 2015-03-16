300c99eac5ef5eacc5a158bea4b578a95fc597df
baasnataliia <basnataliia@gmail.com>
2015-03-15 22:10:09 -0400

    Homepage modules - update

--- BlindRiver/Controllers/HomepageModuleController.cs
+++ BlindRiver/Controllers/HomepageModuleController.cs
@@ -18,5 +18,36 @@ namespace BlindRiver.Controllers
             return View(modules);
         }
 
+        public ActionResult Update(int id)
+        {
+            var modules = objModule.getModuleById(id);
+            if(modules == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(modules);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, homemodule module)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    objModule.commitUpdate(id, module.image_path, module.link_url, module.description, module.link_name);
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
     }
 }
