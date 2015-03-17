5c9058d498e40caaf6cceb72e0397b7393c582a6
dvrs007 <phil@dvrsdesign.com>
2015-03-17 13:26:06 -0400

    added public find doctor
    added public find doctor

---
+++ BlindRiver/Controllers/doctorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BlindRiver.Models;
+
+namespace BlindRiver.Controllers
+{
+    public class doctorController : Controller
+    {
+        allDoctorsClass objDoc = new allDoctorsClass();
+
+
+        public ActionResult Index()
+        {
+            var allDocs = objDoc.getDocs();
+            return View(allDocs);
+        }
+
+    }
+}
---
+++ BlindRiver/Models/allDoctorsClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlindRiver.Models
+{
+    public class allDoctorsClass
+    {
+       doctorsDataContext objDoc = new doctorsDataContext();
+
+        //get all modules from database
+        public IEnumerable<doctor> getDocs()
+        {
+            var allDocs = objDoc.doctors.Select(x => x);
+            return allDocs;
+        }
+
+        public doctor getDocById(int _id)
+        {
+            var singleDoc = objDoc.doctors.SingleOrDefault(x => x.Id == _id);
+            return singleDoc;
+        }
+    }
+}
