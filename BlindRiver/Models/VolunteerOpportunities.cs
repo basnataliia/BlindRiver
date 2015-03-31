44d7a9d8b92660ad73a61688bc8fc1c9288841f3
jaehalee91 <jaehalee91@gmail.com>
2015-03-30 21:26:05 -0400

    Volunteer Opportunities - Public
    Not fully functional at the moment.

---
+++ BlindRiver/Models/VolunteerOpportunities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlindRiver.Models
+{
+    public class VolunteerOpportunities
+    {
+        VolunteerOpportunitiesDataContext objVolOp = new VolunteerOpportunitiesDataContext();
+
+        public IQueryable<VolunteerOpportunities> getVolOps()
+        {
+            var allVolOp = objVolOp.Volunteer_Opportunities.Select(x => x);
+            return allVolOp;
+        }
+
+        public VolunteerOpportunities getVolOpByID(int _id)
+        {
+            var allVolOP = objVolOp.Volunteer_Opportunities.SingleOrDefault(x => x.id == _id);
+            return allVolOP;
+        }
+    }
+}
